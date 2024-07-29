2a749b8921a1029b5702ab51af3dcb8de051ae4b
Zihad Joy <smz.joy73@gmail.com>
2024-07-29 23:05:15 +0600

    Optimized Some Code

--- Day5-Falling Sand/falling_sand.py
+++ Day5-Falling Sand/falling_sand.py
@@ -1,6 +1,6 @@
 from random import randint, choice
 import pygame
-
+from typing import Tuple, List
 
 sand_colors = [
     (194, 178, 128),  # Light Sand
@@ -17,7 +17,7 @@ class Block:
         self.state: bool = False
         self.visited: bool = False
         self.settled: bool = False
-        self.color: tuple[int, int, int] = (-1, -1, -1) # no color
+        self.color: Tuple[int, int, int] = (-1, -1, -1)  # no color
 
     def render(self, surface: pygame.surface) -> None:
         if self.state:
@@ -27,29 +27,35 @@ class Block:
 
     def make_sand(self):
         self.state = True
-        self.color = choice(sand_colors)
+        self.color = choice(seq=sand_colors)
 
-    # def check_pressed(self) -> None:
-    #     if self.rect.collidepoint(pygame.mouse.get_pos()):
-    #         self.state = True
+    def make_air(self):
+        self.state: bool = False
+        self.visited: bool = False
+        self.settled: bool = False
+        self.color: Tuple[int, int, int] = (-1, -1, -1) # no color
 
     @staticmethod
-    def move(cells: list[list['Block']], current_pos: tuple[int, int], next_pos: tuple[int, int]) -> None:
-        curren_cell = cells[current_pos[0]][current_pos[1]]
+    def reset(cells: List[List['Block']]):
+        for cell_row in cells:
+            for cell in cell_row:
+                cell.make_air()
+
+    @staticmethod
+    def move(cells: List[List['Block']], current_pos: Tuple[int, int], next_pos: Tuple[int, int]) -> None:
+        current_cell = cells[current_pos[0]][current_pos[1]]
         next_cell = cells[next_pos[0]][next_pos[1]]
-        curren_cell.state = False  # current cells current position
+        current_cell.state = False  # current cells current position
         next_cell.state = True  # current cells next position
-        next_cell.color = curren_cell.color
-        curren_cell.color = (-1, -1, -1)
+        next_cell.color = current_cell.color
+        current_cell.color = (-1, -1, -1)
         cells[next_pos[0]][next_pos[1]].visited = True
 
     @staticmethod
-    def update(cells: list[list['Block']], row: int, col: int, rows: int, cols: int) -> None:
+    def update(cells: List[List['Block']], row: int, col: int, rows: int, cols: int) -> None:
         current_cell = cells[row][col]
-
         if current_cell.settled:
             return
-
         if current_cell.state and row == rows - 1:
             current_cell.settled = True
             return
@@ -88,7 +94,7 @@ class Block:
                         current_cell.settled = True
 
 
-def pygame_run(window_size: tuple[int, int]) -> tuple[pygame.surface, pygame.time.Clock]:
+def pygame_run(window_size: Tuple[int, int]) -> Tuple[pygame.surface, pygame.time.Clock]:
     pygame.init()
     pygame.display.set_caption("Falling Sand")
     return (pygame.display.set_mode(window_size),
@@ -96,15 +102,15 @@ def pygame_run(window_size: tuple[int, int]) -> tuple[pygame.surface, pygame.tim
 
 
 def main() -> int:
-    width: int = 500
-    height: int = 500
-    block_size: int = 5
+    width: int = 600
+    height: int = 600
+    block_size: int = 4
     rows: int = int(width / block_size)
     cols: int = int(height / block_size)
-    fps: int = 60
+    fps: int = 80
     run: bool = True
 
-    cells: list[list[Block]] = [[
+    cells: List[List[Block]] = [[
         Block(row=row, col=col, size=block_size)
         for col in range(cols)]
         for row in range(rows)]
@@ -118,33 +124,24 @@ def main() -> int:
             if event.type == pygame.QUIT:
                 run = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-                row, col = mouse_y // block_size, mouse_x // block_size
-                if 0 <= row < rows and 0 <= col < cols:
+                mouse_pos = pygame.mouse.get_pos()
+                col = mouse_pos[0] // block_size
+                row = mouse_pos[1] // block_size
+                if 0 <= row < rows and 0 <= col < cols:  # efficiently checking the pressed cell
                     cells[row][col].make_sand()
-
-                # for cell_row in cells:
-                #     for cell in cell_row:
-                #         cell.check_pressed()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    Block.reset(cells)
 
         # testing
-        cells[0][50].make_sand()
+        cells[0][60].make_sand()
 
         # Update
-        for row in range(rows - 1, -1, -1):
-            for col in range(cols - 1, -1, -1):
-                cells[row][col].update(cells, row, col, rows, cols)
-        # row: int = rows - 1
-        # while row >= 0:
-        #     col: int = cols - 1
-        #     while col >= 0:
-        #         Block.update(cells, row, col, rows, cols)
-        #         col -= 1
-        #     row -= 1
-
+        for row in range(rows-1, -1, -1):  # in reverse order
+            for col in range(cols-1, -1, -1):  # in reverse order
+                cells[row][col].update(cells=cells, row=row, col=col, rows=rows, cols=cols)
 
         # Render
-
         window.fill((0, 200, 255))
         for cell_row in cells:
             for cell in cell_row:
